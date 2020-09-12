a1f395140c096d0cd9f2ae0b91373e8e6e262405
Geonhee-LEE <gunhee6392@gmail.com>
2020-09-12 15:11:23 +0900

    Bug fixed for dependency

--- include/waypoint_global_planner/waypoint_global_planner.h
+++ include/waypoint_global_planner/waypoint_global_planner.h
@@ -6,6 +6,7 @@
 #include <costmap_2d/costmap_2d.h>
 #include <nav_core/base_global_planner.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <base_local_planner/world_model.h>
 #include <base_local_planner/costmap_model.h>
 #include <visualization_msgs/MarkerArray.h>
